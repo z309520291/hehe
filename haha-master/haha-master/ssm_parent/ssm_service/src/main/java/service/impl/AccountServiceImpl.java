10c8860883f562ea95a077b3b603a24dcf1172c6
z309520291 <309520291@qq.com>
2018-11-15 19:35:56 +0800

    重新提交

--- haha-master/haha-master/ssm_parent/ssm_service/src/main/java/service/impl/AccountServiceImpl.java
+++ haha-master/haha-master/ssm_parent/ssm_service/src/main/java/service/impl/AccountServiceImpl.java
@@ -3,9 +3,11 @@ package service.impl;
 import dao.AccountDao;
 import domain.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import service.AccountService;
 
+import java.io.File;
 import java.util.List;
 
 @Component("accountService")
@@ -16,6 +18,7 @@ public class AccountServiceImpl implements AccountService {
 
     public List<Account> findAll() {
 
+        ApplicationContext application = new ClassPathXmlApplication(new File("applicationContext.xml"));
         System.out.println("业务层：查询所有账户...");
         List<Account> list = accountDao.findAll();
         return list;
